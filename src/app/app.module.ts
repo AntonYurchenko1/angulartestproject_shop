97f4e2b9939dba65e43d9ecc2d70a923cec3f367
AntonYurchenko1 <anton.yurchenko@gmail.com>
2020-03-09 11:45:12 +0200

    Hometask2, add additional draft modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CartModule } from './cart/cart.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +22,12 @@ import { CartComponent } from './cart/components/cart/cart.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    CartModule,
+    ProductsModule,
+    OrdersModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
